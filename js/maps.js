1cea6c702a245e641bb80fd1cb3e2ba961a3ee1e
LenerWeb <lener20@hotmail.com>
2022-10-13 15:14:49 -0500

    maps.js tu ubicacion

--- js/maps.js
+++ js/maps.js
@@ -1,12 +1,21 @@
+function pizzaDefault() {
+
+  let mapDefault = document.getElementById("map")
+
+  let defaultURL = "https://www.google.com/maps/embed/v1/place?q=place_id:ChIJsXvWDzrPBZERksmlW6uTzRw&key=AIzaSyC1GwuWtjOqNc8mzk0sKlnBGx-_GVD-xXI"
+
+  mapDefault.innerHTML = `<iframe height="350" src="${defaultURL}"></iframe>`
+}
+
 function findMe() {
   let output = document.getElementById("map");
-  
+
   /* verificar si soporta geolocalizacion */
   if (navigator.geolocation) {
-    output.innerHTML = "<p>Tu navegador soporta Geolocalizacion</p>";
+    output.innerHTML = `<p class="ubcOn">Exelente tu navegador soporta Geolocalizacion</p>`;
   } else {
-    output.innerHTML = "<p>Tu navegador no soporta Geolocalizacion</p>";
-  }
+    output.innerHTML = `<p class="ubcOff">No te preocupes aunque tu navegador no soporta Geolocalizacion igual podemos hacer el delivery solo llamanos</p>`;
+  };
 
   /* obtenemos latitud y longitud */
   function localizacion(posicion) {
@@ -15,14 +24,23 @@ function findMe() {
 
     let imgURL = `https://www.google.com/maps/embed/v1/place?q=${latitude},${longitude}&key=AIzaSyC1GwuWtjOqNc8mzk0sKlnBGx-_GVD-xXI`;
 
-    output.innerHTML = `<iframe height="250" width="80" src="${imgURL}"></iframe>`
-    /* output.innerHTML = `<p>Latitud: ${latitude} <br> Longitud: ${longitude} </p>`; */
+    output.innerHTML = `<iframe height="350" src="${imgURL}"></iframe>`
   }
-  function error(){
-    output.innerHTML = "<p>No se pudo obtener tu ubicacion</p>";
+
+  function error() {
+    Swal.fire({
+      title: "Upsss",
+      text: "No se pudo obtener tu ubicacion, por favor Activa tu ubicacion",
+      icon: "Error"
+    })
   }
-  navigator.geolocation.getCurrentPosition(localizacion,error);
+
+  navigator.geolocation.getCurrentPosition(localizacion, error);
 }
+
+pizzaDefault()
+
+
 
 
 
