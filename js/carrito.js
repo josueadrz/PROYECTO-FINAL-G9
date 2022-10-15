725a1afe52cee5d090886b4846be3ae98203d6a5
Juan Pablo Barrientos <jp.barrientos.ro@gmail.com>
2022-10-14 19:02:59 -0500

    Update carrito.js

--- js/carrito.js
+++
@@ -1,9 +0,0 @@
-const detectarBotones = (data) =>{
-    const botones = document.querySelectorAll('.card button')
-
-    botones.forEach(btn =>{
-      btn.addEventListener('click',()=>{
-        console.log('medisteclick')
-      })
-    })
-}
